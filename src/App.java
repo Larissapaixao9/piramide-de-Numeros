fa0b34bcab5988f0e11dfe052e82c89036e207bc
Larissa Paixao <laryssapaixao9@gmail.com>
2023-01-10 15:28:32 -0300

    feat: finish exercise

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int x = 5;
+        String string = "";
+
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=i;j++){
+                string+=i;
+            }
+            string+="\n";
+        }
+
+        System.out.println(string);
     }
 }
